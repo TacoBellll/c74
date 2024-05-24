3120142db566211f07c40f7dabb64990d61beee3
TacoBellll <rishkay796@gmail.com>
2024-05-23 17:57:18 -0700

    Add files via upload

---
+++ SA1/main.py
@@ -0,0 +1,33 @@
+import numpy as np
+import os
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+
+# Create a path variable that holds relative address to the dataset folder
+path = "SA1/Face-image-dataset-small"
+''' 
+Lable the files named 0,1,2 in the path folder as follows
+
+1_0_0_20161219154909149.jpg.chip
+10_0_0_20170110225557604.jpg.chip
+101_1_2_20170105174739309.jpg.chip
+'''
+detector = FaceDetector()
+
+# Iterate through each image i.e img in the variable path
+for img in os.listdir(path):
+    #try:
+    try:
+        print(img)
+        # Print the img
+   
+
+
+    except:
+        print("Error in reading")
+    
+
+
+
+
+
